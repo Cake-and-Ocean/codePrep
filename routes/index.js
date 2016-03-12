e1b7ff352e83a16d9dd68b26a9e8e74e37397abc
Jennifer Bland <ratracegrad@gmail.com>
2016-03-11 20:56:01 -0500

    Removed jade templating language so that we can use html instead.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = process.cwd();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendfile(path + '/public/index.html');
 });
 
 module.exports = router;
