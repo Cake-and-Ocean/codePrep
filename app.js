5e0afa9fc55014720e669a365c0c58e691fdbea3
Jennifer Bland <ratracegrad@gmail.com>
2016-03-11 21:39:47 -0500

    moved server.js from www folder. Added models for challenge and users. Added mongoose to package.json. Added favicon.ico.

--- app.js
+++ app.js
@@ -10,12 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,27 +29,10 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+var path = process.cwd();
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.sendfile(path + '/public/error.html');
 });
 
 
---
+++ models/challenges.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Challenge = new Schema({
+    currentIndex: Number,
+    nextIndex:    Number,
+    title:        String,
+    description:  Array,
+    examples:     Array,
+    results:      Array,
+    tests:        Array,
+    solution:     Array,
+    defaultValue: Array
+});
+
+module.exports = mongoose.model('Challenge', Challenge);
